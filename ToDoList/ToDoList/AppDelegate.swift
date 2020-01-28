c1849ba0608dce210987eac75b24fb1f65cb4862
Vitalii Sidorskyi <vitsky@icloud.com>
2020-01-28 10:16:07 +0200

    Initial
    Initial commit

---
+++ ToDoList/ToDoList/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ToDoList
+//
+//  Created by Vitalii Sydorskyi on 1/28/20.
+//  Copyright © 2020 Vitalii Sydorskyi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Vitalii Sydorskyi on 1/28/20.
+//  Copyright © 2020 Vitalii Sydorskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
